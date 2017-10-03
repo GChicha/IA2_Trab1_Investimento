e20cc009412234cc497600af8565a138898ad3d8
Guilherme Chichanoski <guilherme.chichanoski@gmail.con>
2017-10-03 02:01:47 -0300

    Refatora init do banco

--- main.py
+++ main.py
@@ -10,10 +10,27 @@ def insert_on_bd(cotacao_diaria, cursor):
                    (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
                    , cotacao_diaria)
 
+def init_bd(cursor):
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Cotacoes
+                        (data DATE, cod_bdi TEXT,
+                        cod_neg TEXT, tipo_mer INT,
+                        nome_res TEXT, esp_papel TEXT,
+                        prazo FLOAT, moeda TEXT,
+                        preco_abertura FLOAT, preco_max FLOAT,
+                        preco_min FLOAT, preco_med FLOAT,
+                        preco_ultimo FLOAT, preco_melhor_compra FLOAT,
+                        preco_melhor_venda FLOAT, total_negociado FLOAT,
+                        quantidade_total FLOAT, volume_total FLOAT,
+                        preco_exercicio FLOAT, indicador_correcao TEXT,
+                        data_venc DATE, fator_cotacao INT,
+                        preco_pontos FLOAT, cod_isin TEXT,
+                        dis_papel INT,
+                        PRIMARY KEY (data, cod_bdi, cod_neg))''')
+
 def main():
     parser = argparse.ArgumentParser(description="Extrai informações do arquivo bovespa")
     parser.add_argument('--entrada', metavar='FILE', nargs='*', type=argparse.FileType('r'),
-                        help="Arquivos de entrada")
+                        help="Arquivos de entrada", default=[])
     parser.add_argument('--bdname', metavar='BD', nargs='?', default=":memory:",
                         help="Nome do arquivo do banco de dados se nenhum nome for passado o\
                               banco não será persistido")
@@ -30,23 +47,12 @@ def main():
     if parsed.debug:
         banco_dados.set_trace_callback(print)
 
+    if not parsed.entrada and parsed.bdname == ":memory:":
+        raise Exception("Pelo menos uma entrada ou um banco devem ser fornecido")
+
     cursor_bd = banco_dados.cursor()
 
-    cursor_bd.execute('''CREATE TABLE IF NOT EXISTS Cotacoes
-                        (data DATE, cod_bdi TEXT,
-                        cod_neg TEXT, tipo_mer INT,
-                        nome_res TEXT, esp_papel TEXT,
-                        prazo FLOAT, moeda TEXT,
-                        preco_abertura FLOAT, preco_max FLOAT,
-                        preco_min FLOAT, preco_med FLOAT,
-                        preco_ultimo FLOAT, preco_melhor_compra FLOAT,
-                        preco_melhor_venda FLOAT, total_negociado FLOAT,
-                        quantidade_total FLOAT, volume_total FLOAT,
-                        preco_exercicio FLOAT, indicador_correcao TEXT,
-                        data_venc DATE, fator_cotacao INT,
-                        preco_pontos FLOAT, cod_isin TEXT,
-                        dis_papel INT,
-                        PRIMARY KEY (data, cod_bdi, cod_neg))''')
+    init_bd(cursor_bd)
 
     for arquivo in parsed.entrada:
         # Só insere no banco se a empresa é objeto de avaliação
